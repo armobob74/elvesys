d93fd50f3ed729271ee52537acafbf1021b28964
Armon <armon@nudgex.net>
2024-01-23 14:34:48 -0800

    pman-ified the new pman endpoints

--- pman.py
+++ pman.py
@@ -1,28 +1,47 @@
 from flask import Blueprint, request, render_template
+import json
 from ob1_control import ob1_state_control
 from mux_control import mux_state_control
 from flowmeter_control import get_density_and_flow
+from dist_control import dist_state_control
 
 pman = Blueprint('pman', __name__)
 
 @pman.route("/mux", methods=["POST"])
 def mux():
-    com_port_mux = request.form["com_port_mux"]
-    curr_state = [int(request.form[f"curr_state_{i}"]) for i in range(8)]
-    desired_state = [int(request.form[f"desired_state_{i}"]) for i in range(8)]
-    mux_state_control(com_port_mux, curr_state, desired_state)
+    d = json.loads(request.data)
+    args = d['args']
+    com_port = args[0]
+    curr_state = args[1]
+    desired_state = args[2]
+    mux_state_control(com_port, curr_state, desired_state)
     return {'state':'ok','message':'Mux'}
 
 @pman.route("/ob1", methods=["POST"])
 def ob1():
-    com_port_ob1 = request.form["com_port_ob1"]
-    channel_to_initialize = int(request.form["channel_to_initialize"])
-    pressure_to_set = float(request.form["pressure_to_set"])
-    ob1_state_control(com_port_ob1, channel_to_initialize, pressure_to_set)
+    d = json.loads(request.data)
+    args = d['args']
+    com_port = args[0]
+    channel_to_initialize = args[1]
+    pressure_to_set = args[2]
+    ob1_state_control(com_port, channel_to_initialize, pressure_to_set)
     return {'state':'ok','message':'OB1'}
 
 @pman.route("/density-and-flow", methods=["POST"])
 def densityAndFlow():
-    com_port = ''
+    d = json.loads(request.data)
+    args = d['args']
+    com_port = args[0]
     density, flow = get_density_and_flow(com_port)
     return {'state':'ok','message':{'density':density,'flow':flow}}
+
+@pman.route("/dist", methods=["POST"])
+def dist():
+    d = json.loads(request.data)
+    args = d['args']
+    com_port = args[0]
+    initial_set_valveID = args[1]
+    desired_set_valveID = args[2]
+    dist_state_control(com_port, initial_set_valveID, desired_set_valveID)
+    return {'state':'ok','message':'Valving'}
+
