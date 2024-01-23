cfe5a557f7d2d0496a54a36c7c0a3f7a73ec4494
Armon <armon@nudgex.net>
2024-01-23 14:46:56 -0800

    refactored pman with a new decorator

--- pman.py
+++ pman.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, render_template
+from functools import wraps
 import json
 from ob1_control import ob1_state_control
 from mux_control import mux_state_control
@@ -7,41 +8,39 @@ from dist_control import dist_state_control
 
 pman = Blueprint('pman', __name__)
 
+def extract_pman_args(f):
+    """
+    Extract the pman args from the request and plug them into the decorated function
+    Use as a decorator if desired
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        data = json.loads(request.data)
+        args = data.get('args',[])
+        return f(*args)
+    return decorated_function
+
 @pman.route("/mux", methods=["POST"])
-def mux():
-    d = json.loads(request.data)
-    args = d['args']
-    com_port = args[0]
-    curr_state = args[1]
-    desired_state = args[2]
+@extract_pman_args
+def mux(com_port, curr_state, desired_state):
     mux_state_control(com_port, curr_state, desired_state)
     return {'state':'ok','message':'Mux'}
 
 @pman.route("/ob1", methods=["POST"])
-def ob1():
-    d = json.loads(request.data)
-    args = d['args']
-    com_port = args[0]
-    channel_to_initialize = args[1]
-    pressure_to_set = args[2]
+@extract_pman_args
+def ob1(com_port, channel_to_initialize, pressure_to_set):
     ob1_state_control(com_port, channel_to_initialize, pressure_to_set)
     return {'state':'ok','message':'OB1'}
 
 @pman.route("/density-and-flow", methods=["GET"])
-def densityAndFlow():
-    d = json.loads(request.data)
-    args = d['args']
-    com_port = args[0]
+@extract_pman_args
+def densityAndFlow(com_port):
     density, flow = get_density_and_flow(com_port)
     return {'state':'ok','message':{'density':density,'flow':flow}}
 
 @pman.route("/dist", methods=["POST"])
-def dist():
-    d = json.loads(request.data)
-    args = d['args']
-    com_port = args[0]
-    initial_set_valveID = args[1]
-    desired_set_valveID = args[2]
+@extract_pman_args
+def dist(com_port, initial_set_valveID, desired_set_valveID):
     dist_state_control(com_port, initial_set_valveID, desired_set_valveID)
     return {'state':'ok','message':'Valving'}
 
