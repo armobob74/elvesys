e9823950ea09b56f61593cb395d6f94f0a87e7b3
Armon <armon@nudgex.net>
2024-01-26 15:02:52 -0800

    Elvesys now gets device COM port by looking up device name in the config.json file

--- pman.py
+++ pman.py
@@ -11,6 +11,9 @@ config = {}
 with open('config.json') as f:
     config = json.load(f)
 
+name_to_port = config['name_to_port']
+
+
 pman = Blueprint('pman', __name__)
 
 def extract_pman_args(f):
@@ -27,23 +30,27 @@ def extract_pman_args(f):
 
 @pman.route("/mux", methods=["POST"])
 @extract_pman_args
-def mux(com_port, curr_state, desired_state):
+def mux(device_name, curr_state, desired_state):
+    com_port = name_to_port[device_name]
     mux_state_control(com_port, curr_state, desired_state)
     return {'state':'ok','message':'Mux'}
 
 @pman.route("/ob1", methods=["POST"])
 @extract_pman_args
-def ob1(com_port, channel_to_initialize, pressure_to_set):
+def ob1(device_name, channel_to_initialize, pressure_to_set):
+    com_port = name_to_port[device_name]
     ob1_state_control(com_port, channel_to_initialize, pressure_to_set)
     return {'state':'ok','message':'OB1'}
 
-@pman.route("/density-and-flow/<string:com_port>", methods=["GET"])
-def densityAndFlow(com_port):
+@pman.route("/density-and-flow/<string:device_name>", methods=["GET"])
+def densityAndFlow(device_name):
+    com_port = name_to_port[device_name]
     density, flow = get_density_and_flow(com_port)
     return {'state':'ok','message':{'density':density,'flow':flow}}
 
 @pman.route("/dist", methods=["POST"])
 @extract_pman_args
-def dist(com_port, initial_set_valveID, desired_set_valveID):
+def dist(device_name, initial_set_valveID, desired_set_valveID):
+    com_port = name_to_port[device_name]
     dist_state_control(com_port, initial_set_valveID, desired_set_valveID)
     return {'state':'ok','message':'Valving'}
